8466dd6d2907aa3659e98d569b02099d00ee492f
Dustyn Brunse <dustyn79087@gmail.com>
2023-06-05 00:14:21 -0500

    styling and image testing for projects.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Projects from './screens/Projects';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+import './App.css';
+
 
 function App() {
   return (
--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -1,55 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ReactMarkdown from 'react-markdown';
-import { Container, Row, Col, Card, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { Container, Col, Card, OverlayTrigger, Tooltip } from 'react-bootstrap';
+
+import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Projects() {
-  const [projects, setProjects] = useState([]);
+    const [projects, setProjects] = useState([]);
 
-  useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const response = await axios.get('http://localhost:3001/projects');
-        setProjects(response.data);
-      } catch (error) {
-        console.error('Failed to fetch projects:', error);
-      }
-    };
+    useEffect(() => {
+        const fetchProjects = async () => {
+            try {
+                const response = await axios.get('http://localhost:3001/projects');
+                setProjects(response.data);
+            } catch (error) {
+                console.error('Failed to fetch projects:', error);
+            }
+        };
 
-    fetchProjects();
-  }, []);
+        fetchProjects();
+    }, []);
 
-  return (
-    <Container>
-      <Row className="justify-content-md-center">
-        <h1>My Projects</h1>
-        <p>You can access the code for these projects on my GitHub profile.</p>
-      </Row>
-      <Row xs={1} md={2} className="g-4">
-        {projects.map((project, idx) => (
-          <Col key={idx}>
-          <a href={project.html_url} target="_blank" rel="noreferrer">
-              <OverlayTrigger
-                overlay={
-                  <Tooltip id={`tooltip-${idx}`}>
-                    <ReactMarkdown>{project.readme}</ReactMarkdown>
-                  </Tooltip>
-                }
-              >
-                <Card className="h-100">
-                  <Card.Img variant="top" src={`https://raw.githubusercontent.com/${project.owner.login}/${project.name}/master/snapshot.png`} />
-                  <Card.Body>
-                    <Card.Title>{project.name}</Card.Title>
-                  </Card.Body>
-                </Card>
-              </OverlayTrigger>
-            </a >
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  );
+    return (
+        <Container>
+            <Col className="page-title">
+                <h1>My Projects</h1>
+                <br />
+                <p>Here are some of my projects! Click on any one of the projects that you would like to see, and you will be navigated to my Github Repository!</p>
+            </Col>
+            <div xs={1} md={2} className="projects-container">
+                {projects.map((project, idx) => (
+                    <Col key={idx}>
+                        <a href={project.html_url} target="_blank" rel="noreferrer">
+                            <OverlayTrigger
+                                overlay={
+                                    <Tooltip id={`tooltip-${idx}`}>
+                                        <ReactMarkdown>{project.readme}</ReactMarkdown>
+                                    </Tooltip>
+                                }
+                            >
+                                <Card className='project-card'>
+                                    <Card.Img 
+                                        className='proj-img'
+                                        variant="top"
+                                        src={`https://raw.githubusercontent.com/${project.owner.login}/${project.name}/master/snapshot.png`}
+                                        />
+                                    <Card.Body>
+                                        <Card.Title classname='project-title'>{project.name}</Card.Title>
+                                    </Card.Body>
+                                </Card>
+                            </OverlayTrigger>
+                        </a >
+                    </Col>
+                ))}
+            </div>
+        </Container>
+    );
 }
 
 export default Projects;
